e0bd2c95d6e930e90241901f86519ac6423688c8
iperales9 <iperales@disid.com>
2025-04-07 11:59:57 +0200

    Sección 06 Spring Boot Test de Controladores

---
+++ Section 06 Spring Boot Test de Controladores con MockMvc/springboot_test/src/main/java/com/ivan/test/springboot/app/springboot_test/services/CuentaService.java
@@ -0,0 +1,22 @@
+package com.ivan.test.springboot.app.springboot_test.services;
+
+import com.ivan.test.springboot.app.springboot_test.models.Cuenta;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface CuentaService {
+
+    List<Cuenta> findAll();
+
+    Cuenta findById(Long id);
+
+    Cuenta save(Cuenta cuenta);
+
+    int revisarTotalTransferencias(Long bancoId);
+
+    BigDecimal revisarSaldo(Long cuentaId);
+
+    void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto,
+                    Long bancoId);
+}
